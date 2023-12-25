897cd387da1c9663b14d07b93a4939ad9504fae6
Sir-SpongeBob <23226080@life.hkbu.edu.hk>
2023-12-25 22:20:13 +0800

    update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,63 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+vim.o.cindent = true
+-- vim.o.smartindent = true
+
+-- vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.linebreak = true
+vim.o.scrolloff = 15
+vim.wo.rnu = true
+
+return {
+  {
+    "windwp/nvim-autopairs",
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require("nvim-autopairs").setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end,
+  },
+
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 900,
+    config = function()
+      vim.cmd.colorscheme 'catppuccin-mocha'
+    end,
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'catppuccin-mocha',
+        component_separators = '|',
+        section_separators = '',
+      },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            newfile_status = true,
+            path = 4,
+          }
+        }
+      },
+    },
+  },
+}

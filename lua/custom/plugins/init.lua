98e02629194f88f42cb4de9915f21f1ac348f6eb
Sir-SpongeBob <23226080@life.hkbu.edu.hk>
2024-01-05 21:08:10 +0800

    add nvim-neo-tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -38,6 +38,40 @@ return {
   },
 
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      {
+        's1n7ax/nvim-window-picker',
+        version = '*',
+        config = function()
+            require 'window-picker'.setup({
+                filter_rules = {
+                    include_current_win = false,
+                    autoselect_one = true,
+                    -- filter using buffer options
+                    bo = {
+                        -- if the file type is one of following, the window will be ignored
+                        filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+                        -- if the buffer type is one of following, the window will be ignored
+                        buftype = { 'terminal', "quickfix" },
+                    },
+            },
+        })
+        end,
+      },
+    },
+    config = function ()
+      vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+      require('neo-tree').setup {}
+    end,
+  },
+
+  {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 900,
